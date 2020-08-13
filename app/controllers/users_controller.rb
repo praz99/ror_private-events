7b53eb6a0ab62494851e7492f740d3ac0852753a
praz <t.prazwal@gmail.com>
2020-08-13 16:23:17 +0200

    add authentication for user add login with only username add signup page generate session controller use session for authentication add views for various methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:show]
   def new
     @user = User.new
   end
@@ -8,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to Private Events!'
       redirect_to @user
     else
