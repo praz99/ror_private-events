82f3dad9972bb3c0cb42986ceb40fa22183d5e87
praz <t.prazwal@gmail.com>
2020-08-13 15:18:34 +0200

    add views for user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
-  def new; end
-
-  def create
-    session[:username] = params[:username]
-    redirect_to root_path
+  def new
+    @user = User.new
   end
 
-  def show; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'Welcome to Private Events!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end
 end
