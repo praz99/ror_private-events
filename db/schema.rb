5653f6e380ed56b19dfe71b6809d203fe3e6f228
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2020-08-14 09:29:54 +0000

    Generate the event controller and migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_090718) do
+ActiveRecord::Schema.define(version: 2020_08_13_161826) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "events_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
