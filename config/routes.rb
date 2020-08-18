9477ef2eee056f9283e82ba7e880af8167346726
praz <t.prazwal@gmail.com>
2020-08-18 17:57:02 +0200

    change index page for events make index page show all the events make user show page display all the events created by user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index new create show]
-  resources :events, only: %i[new index show create]
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  root 'sessions#new'
+  resources :events, only: %i[new index show create] do
+    resources :event_attendings
+  end
+  root 'events#index'
 end
