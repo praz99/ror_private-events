1efed3cad6fb6d5188620edb221abbcb58b57597
praz <t.prazwal@gmail.com>
2020-08-19 11:51:45 +0200

    add new method on event_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:index, :show, :new, :create]
+  before_action :set_current_user, only: [:show, :index, :new, :attended_event, :add_attended_event]
   def index
     @events = Event.all
   end
@@ -22,11 +23,26 @@ class EventsController < ApplicationController
     end
   end
 
+  def attended_event
+    @events = Event.all
+  end
+
+  def add_attended_event
+    event_ids = params[:event_ids]
+    attended_events = event_ids.collect { |id| Event.find(id) }
+    @current_user.attended_events = attended_events
+  end
+
   private
 
   def set_event
     # @event = Event.find_by_id(params[:creator_id])
     @event = Event.find_by(id: params[:creator_id])
+  end
+
+  def set_current_user
+    redirect_to signup_path unless current_user
+    @current_user = current_user
   end
 
   def event_params
