45e4b7128956d9ec377ab414e24accdc56aca820
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2020-08-20 13:53:24 +0000

    Pass the event parameter to take route path in user/show.html

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find_by(params[:id])
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 
   has_many :event_attendings, foreign_key: :attended_events_id, dependent: :destroy
-  has_many :attendees, through: :event_attendings, source: :event_attendee
+  has_many :attendees, through: :event_attendings, source: :event_attendee, dependent: :destroy
 
   scope :previous_event, -> { where('events_date < ?', Date.today) }
   scope :upcoming_event, -> { where('events_date >= ?', Date.today) }
